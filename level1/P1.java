eb643e3de9a601f8fc9e44b27163d553ac60d72d
Suryansh <suryansh.sg.0777@gmail.com>
2026-04-13 13:48:04 +0530

    level 1

---
+++ level1/P1.java
@@ -0,0 +1,31 @@
+
+package level1;
+import java.util.Scanner;
+
+public class P1 {
+
+    // Method to calculate simple interest
+    public static double calculateSI(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter Rate: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter Time: ");
+        double time = sc.nextDouble();
+
+        double si = calculateSI(principal, rate, time);
+
+        System.out.println("The Simple Interest is " + si +
+                " for Principal " + principal +
+                ", Rate of Interest " + rate +
+                " and Time " + time);
+    }
+}
---
+++ level1/P2.java
@@ -0,0 +1,21 @@
+package level1;
+import java.util.Scanner;
+
+public class P2 {
+
+    // Method to calculate handshakes
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int result = calculateHandshakes(n);
+
+        System.out.println("Maximum number of handshakes: " + result);
+    }
+}
